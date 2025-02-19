1733e59f04a24cf9646ae20b632e5259c34477ba
Jopsxl <Josefernandoperez07@gmail.com>
2025-02-19 11:22:24 -0600

    Delete Introduccion a JavaScript directory

--- Introduccion a JavaScript/Año de nacimiento.js
+++
@@ -1,4 +0,0 @@
-let a,b,c; 
-a=parseFloat(prompt("Ingrese su año de nacimiento"));
-b=2025-a
-alert("Su edad es"+b)
--- Introduccion a JavaScript/igss.js
+++
@@ -1,6 +0,0 @@
-let a,b,c,d;
-a=parseFloat(prompt("Ingrese su sueldo"));
-b=a*0.0483
-c=a*0.01
-alert("De IGGS se paga " +b)
-alert("De IRTRA se paga " +c)
